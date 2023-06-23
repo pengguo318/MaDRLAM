511680ca4fc57a9f469df14121805a92bea26d02
parsaeisa <parsaeisa@github.com>
2023-06-23 16:06:37 +0330

    add empty lines to remove pyCharm warnings

--- agent_utils.py
+++ agent_utils.py
@@ -1,17 +1,19 @@
 from torch.distributions.categorical import Categorical
 
-
 """Random selection strategy and greedy selection strategy"""
+
 
 def select_action(p):
     dist = Categorical(p.squeeze())
     s = dist.sample()
     return s
 
+
 def greedy_select_action(p):##(batch,n_j*2,1)
     _, index = p.squeeze().max(1)
     # action = candidate[index]
     return index
+
 
 def sample_select_action(p):
     dist = Categorical(p.squeeze())
